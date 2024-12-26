fb67140b12e191aebd437ea548135db0d7b5f195
Sandeep-XD <sanbofficial@gmail.com>
2024-12-26 17:29:21 +0530

    tailwind initialized

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from '../../components/Cards/card1/Card.jsx'
+const Home = () => {
+  return (
+    <div>
+        <Card />
+    </div>
+  )
+}
+
+export default Home
