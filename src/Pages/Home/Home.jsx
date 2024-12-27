6faa4fc894ddda9be9ae39acebbb89096ca024fd
Sandeep-XD <sanbofficial@gmail.com>
2024-12-27 18:21:11 +0530

    working

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,27 +1,13 @@
-import React, { useState } from 'react'
-import Card from '../../components/Cards/card1/Card.jsx'
+import Body from '../../layouts/Body/body1/Body'
 const Home = () => {
-  const [cards, setCard] = useState({
-    title: "How to make a website using HTML CSS Bootstrap | Website Design Tutorial",
-    views: "1.5M",
-    channel: "Easy Tutorials",
-    uploadTime: "1 year",
-    url: "#",
-    userLink: "#",
-    thumbnailPath: "./src/assets/Posters/Card-images/card1/card-3.jpg",
-    logoPath: "./src/assets/Posters/card-images/card1/logo.png"
-  })
   return (
-    <div className='cards'>
-        <Card title={cards.title}
-          views={cards.views} channel={cards.channel}
-          uploadTime= {cards.uploadTime}
-          url= {cards.url}
-          userLink= {cards.userLink}
-          thumbnailPath= {cards.thumbnailPath}
-          logoPath= {cards.logoPath}
-        />
-    </div>
+    <>
+      <div className="container">
+        <div className="body">
+          <Body/>
+        </div>
+      </div>
+    </>
   )
 }
 
