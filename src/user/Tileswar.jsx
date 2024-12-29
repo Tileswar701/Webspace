c9d6f71bb95df0c57b210675a6e8f96b9e2d2806
Sandeep-XD <sanbofficial@gmail.com>
2024-12-29 23:37:35 +0530

    component added in workspace

--- src/user/Tileswar.jsx
+++ src/user/Tileswar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Form from '../components/Forms/form1/Form'
 const Tileswar = () => {
   return (
     <>
@@ -9,6 +9,9 @@ const Tileswar = () => {
               <h1>This is Tileswar's Workspace</h1>
               <p>Tileswar's Workspace is the place where San can work on his projects</p>
             </div>
+          </div>
+          <div className="workspace-content flex justify-center p-4">
+            <Form />
           </div>
       </div>
     </>
